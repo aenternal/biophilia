15b3070d0a56bfaa8b897376fa9ccd348c445922
aenternal <kotikote875@gmail.com>
2024-09-19 18:22:18 +0300

    started blast reengineering

--- internal/domain/entities/blast.go
+++ internal/domain/entities/blast.go
@@ -1,5 +1,7 @@
 package entities
 
+import "reflect"
+
 type SearchRequest struct {
 	Sequence string `json:"sequence"`
 }
@@ -10,4 +12,30 @@ type BlastRequest struct {
 	Sequence string `json:"sequence"`
 	SType    string `json:"stype"`
 	Email    string `json:"email"`
+}
+
+func NewBlastRequest(sequence string) BlastRequest {
+	return BlastRequest{
+		Program:  "blastn",
+		Database: "ipdmhcgen",
+		Sequence: sequence,
+		SType:    "dna",
+		Email:    "mhc@alleles.org",
+	}
+}
+
+func (request *BlastRequest) Map() map[string]string {
+	result := make(map[string]string)
+	val := reflect.ValueOf(request)
+
+	if val.Kind() == reflect.Struct {
+
+		for i := 0; i < val.NumField(); i++ {
+			fieldName := val.Type().Field(i).Name
+			fieldValue := val.Field(i).String()
+			result[fieldName] = fieldValue
+		}
+	}
+
+	return result
 }
