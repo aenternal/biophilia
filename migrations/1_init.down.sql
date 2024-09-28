e238fce24a95acdf1b681deb541098b745cf0bc9
aenternal <kotikote875@gmail.com>
2024-09-28 15:01:18 +0300

    fixed migrations

--- migrations/1_init.down.sql
+++ migrations/1_init.down.sql
@@ -1,17 +1,2 @@
-CREATE TABLE IF NOT EXISTS biomolecule (
-                             id SERIAL PRIMARY KEY,
-                             type VARCHAR(255),
-                             name VARCHAR(255),
-                             sequence TEXT,
-                             description TEXT,
-                             created_at TIMESTAMP DEFAULT NOW(),
-                             updated_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS biomolecule_files (
-                                  id SERIAL PRIMARY KEY,
-                                  biomolecule_id INT REFERENCES biomolecule(id),
-                                  file_url TEXT,
-                                  file_type VARCHAR(50),
-                                  uploaded_at TIMESTAMP DEFAULT NOW()
-);
+DROP TABLE IF EXISTS biomolecule_files;
+DROP TABLE IF EXISTS biomolecule;
